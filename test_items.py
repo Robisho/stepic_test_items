616166f8cfd522c4e6d0a1def8d190689ea3281d
Robisho <50827400+Robisho@users.noreply.github.com>
2020-04-20 15:02:05 +0800

    create file

---
+++ test_items.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_guest_add_to_basket(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector('button.btn-add-to-basket')
+
+    assert 'Yes! Button was found!', 'Button not found'
