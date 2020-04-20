5d51fe7ab22cc1af6fa5c0c0aeb33ea37c126c32
Robisho <50827400+Robisho@users.noreply.github.com>
2020-04-20 15:09:50 +0800

    add time.sleep

--- test_items.py
+++ test_items.py
@@ -7,6 +7,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_add_to_basket(browser):
     browser.get(link)
+    time.sleep(30)
     browser.find_element_by_css_selector('button.btn-add-to-basket')
 
     assert 'Yes! Button was found!', 'Button not found'
