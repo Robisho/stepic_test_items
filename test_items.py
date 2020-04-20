1dcbf4dbb837015c0d25be1e374ff8b18ea965d5
Robisho <50827400+Robisho@users.noreply.github.com>
2020-04-20 15:23:40 +0800

    update assert

--- test_items.py
+++ test_items.py
@@ -9,6 +9,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_guest_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_css_selector('button.btn-add-to-basket')
 
-    assert 'Yes! Button was found!', 'Button not found'
+    button = len(browser.find_elements_by_css_selector('button.btn-add-to-basket'))
+    assert button > 0, 'НЕ НАШЕЛ КНОПКУ!'
